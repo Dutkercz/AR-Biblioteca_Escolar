85eab858be14c5620a081374ca21ac9059ca6fc0
Dutkercz <dutkercz@gmail.com>
2026-06-02 13:07:09 -0300

    feat: commit inicial desafio Biblioteca, com dependências principais definidas.

---
+++ src/test/java/dutkercz/biblioteca/DesafioBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package dutkercz.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
