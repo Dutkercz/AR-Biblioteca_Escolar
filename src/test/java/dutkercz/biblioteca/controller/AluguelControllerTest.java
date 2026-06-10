3574e50d05ebf2e5e6df529089a98ab80381a06e
Dutkercz <dutkercz@gmail.com>
2026-06-10 10:53:47 -0300

    feat: desenvolvido testes de integração para AluguelController, adicionado uma classe Factory para auxiliar nos testes

---
+++ src/test/java/dutkercz/biblioteca/controller/AluguelControllerTest.java
@@ -0,0 +1,155 @@
+package dutkercz.biblioteca.controller;
+
+import dutkercz.biblioteca.controller.factory.FactoryHelper;
+import dutkercz.biblioteca.dto.aluguel.AluguelRequestDto;
+import dutkercz.biblioteca.model.enums.AluguelStatus;
+import dutkercz.biblioteca.repository.AluguelRepository;
+import dutkercz.biblioteca.repository.AutorRepository;
+import dutkercz.biblioteca.repository.LivroRepository;
+import dutkercz.biblioteca.repository.LocatarioRepository;
+import dutkercz.biblioteca.service.AluguelService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@ActiveProfiles("test")
+@Transactional //faz um rollback ao final de cada teste
+class AluguelControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AluguelRepository aluguelRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @Autowired
+    private LocatarioRepository locatarioRepository;
+
+    @Autowired
+    private AluguelService aluguelService;
+
+    @Autowired
+    private JacksonTester<AluguelRequestDto> jsonRequestDto;
+
+    @Test
+    @DisplayName("Deve alugar um livro com sucesso")
+    void alugarLivro() throws Exception {
+        livroRepository.save(FactoryHelper.createLivro());
+        var locatario = locatarioRepository.save(FactoryHelper.createLocatario());
+        var livro =  livroRepository.save(FactoryHelper.createLivro());
+        var requestDto = FactoryHelper.createAluguelRequestDto(locatario.getId(), livro.getId());
+
+        mockMvc.perform(post("/api/alugueis")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonRequestDto.write(requestDto).getJson()))
+               .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.livros[0].id").value(livro.getId()))
+               .andExpect(jsonPath("$.dataRetirada").value(LocalDate.now().toString()))
+               .andExpect(jsonPath("$.locatario.id").value(locatario.getId()))
+               .andExpect(jsonPath("$.livros.length()").value(1))
+               .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("Deve listar corretamente uma lista de alugueis ativos e finalizados")
+    void listarAlugueis() throws Exception {
+        autorRepository.save(FactoryHelper.createAutor());
+        var locatario =  locatarioRepository.save(FactoryHelper.createLocatario());
+        var livro =  livroRepository.save(FactoryHelper.createLivro());
+
+        var livros = aluguelRepository.saveAll(FactoryHelper.listaAluguel(locatario, List.of(livro)));
+
+        mockMvc.perform(get("/api/alugueis"))
+                .andExpect(jsonPath("$.content.length()").value(2))
+                .andExpect(jsonPath("$.content[0].id").value(livros.get(0).getId()))
+                .andExpect(jsonPath("$.content[1].id").value(livros.get(1).getId()))
+                .andDo(print());
+                //alugueis cancelados não entram nesse request por isso o size é 2, mas a lista do factory salva 3
+    }
+
+    @Test
+    @DisplayName("Deve devolver status 200 quando a devolução for bem sucedida")
+    void devolverLivros() throws Exception {
+        autorRepository.save(FactoryHelper.createAutor());
+        var locatario =  locatarioRepository.save(FactoryHelper.createLocatario());
+        var livro =  livroRepository.save(FactoryHelper.createLivro());
+        var aluguel = aluguelRepository.save(FactoryHelper.createAluguel(locatario, List.of(livro)));
+
+        mockMvc.perform(put("/api/alugueis/devolver/"+aluguel.getId()))
+                .andExpect(jsonPath("$.id").value(aluguel.getId()))
+                .andExpect(jsonPath("$.dataDevolucao").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.locatario.id").value(locatario.getId()))
+                .andExpect(jsonPath("$.status").value(AluguelStatus.FINALIZADO.toString()))
+                .andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    @DisplayName("Deve deletar um aluguel com sucesso quando status for ATIVO")
+    void deveDeletarAluguelComSucesso() throws Exception {
+        autorRepository.save(FactoryHelper.createAutor());
+        var locatario =  locatarioRepository.save(FactoryHelper.createLocatario());
+        var livro =  livroRepository.save(FactoryHelper.createLivro());
+        var aluguel = aluguelRepository.save(FactoryHelper.createAluguel(locatario, List.of(livro)));
+
+        mockMvc.perform(delete("/api/alugueis/"+aluguel.getId()))
+               .andExpect(status().isOk()).andDo(print());
+        Assertions.assertFalse(aluguelRepository.existsById(aluguel.getId()));
+    }
+
+    @Test
+    @DisplayName("Deve falhar ao tentar deletar um aluguel com status FINALIZADO")
+    void deveFalharAoDeletarAluguel() throws Exception {
+        autorRepository.save(FactoryHelper.createAutor());
+        var locatario =  locatarioRepository.save(FactoryHelper.createLocatario());
+        var livro =  livroRepository.save(FactoryHelper.createLivro());
+        var aluguel = FactoryHelper.createAluguel(locatario, List.of(livro));
+        aluguel.setStatus(AluguelStatus.FINALIZADO);
+        aluguelRepository.save(aluguel);
+
+        mockMvc.perform(delete("/api/alugueis/"+aluguel.getId()))
+               .andExpect(status().isBadRequest()).andDo(print());
+        Assertions.assertTrue(aluguelRepository.existsById(aluguel.getId()));
+    }
+
+    @Test
+    void livrosAlugadosPorLocatario() throws Exception {
+        autorRepository.save(FactoryHelper.createAutor());
+        var locatario =  locatarioRepository.save(FactoryHelper.createLocatario());
+        var livro =  livroRepository.save(FactoryHelper.createLivro());
+        aluguelRepository.save(FactoryHelper.createAluguel(locatario, List.of(livro)));
+
+        mockMvc.perform(get("/api/alugueis/locatario/"+locatario.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(livro.getId()))
+                .andExpect(jsonPath("$[0].ISBN").value(livro.getISBN()))
+                .andExpect(jsonPath("$.length()").value(1));
+    }
+}
---
+++ src/test/java/dutkercz/biblioteca/controller/factory/FactoryHelper.java
@@ -0,0 +1,84 @@
+package dutkercz.biblioteca.controller.factory;
+
+import dutkercz.biblioteca.dto.aluguel.AluguelRequestDto;
+import dutkercz.biblioteca.dto.autor.AutorRequestDto;
+import dutkercz.biblioteca.dto.livro.LivroRequestDto;
+import dutkercz.biblioteca.dto.locatario.LocatarioRequestDto;
+import dutkercz.biblioteca.mapper.*;
+import dutkercz.biblioteca.model.Aluguel;
+import dutkercz.biblioteca.model.Autor;
+import dutkercz.biblioteca.model.Livro;
+import dutkercz.biblioteca.model.Locatario;
+import dutkercz.biblioteca.model.enums.AluguelStatus;
+import dutkercz.biblioteca.model.enums.GeneroEnum;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+
+public class FactoryHelper {
+
+    private static AluguelMapper aluguelMapper = new AluguelMapperImpl();
+    private static LocatarioMapper locatarioMapper = new LocatarioMapperImpl();
+    private static LivroMapper livroMapper = new LivroMapperImpl();
+    private static AutorMapper autorMapper = new AutorMapperImpl();
+
+    public static AluguelRequestDto createAluguelRequestDto(Long locatarioId, Long livroId) {
+       return new AluguelRequestDto(
+                    List.of(livroId), locatarioId,
+                LocalDate.now().plusDays(2));
+    }
+
+    public static LocatarioRequestDto createLocatarioRequestDto(){
+        return new LocatarioRequestDto("Locatario", GeneroEnum.FEMININO, "55999009988",
+                        "email@email.com",LocalDate.of(2000, 1, 1),
+                        "14725836911");
+    }
+
+    public static LivroRequestDto createLivroRequestDto(){
+        Random random = new Random();
+        int finalISBN = random.nextInt(100,1000);
+        return new LivroRequestDto("Livro1", "1234567891"+finalISBN,
+                LocalDate.of(1950, 1,1), List.of(1L));
+    }
+
+    public static AutorRequestDto createAutorRequestDto(){
+        return new AutorRequestDto("Autor1", GeneroEnum.NAO_BINARIO,
+                LocalDate.of(1950, 1, 1),"12345678912");
+    }
+
+    public static Autor createAutor(){
+        return autorMapper.toEntity(createAutorRequestDto());
+    }
+
+    public static Livro createLivro(){
+        return livroMapper.toEntity(createLivroRequestDto());
+    }
+
+    public static Locatario createLocatario(){
+       return locatarioMapper.toEntity(createLocatarioRequestDto());
+    }
+
+    public static Aluguel createAluguel(Locatario locatario, List<Livro> livros) {
+
+        var aluguel = aluguelMapper.toEntity(
+                createAluguelRequestDto(locatario.getId(),livros.get(0).getId()),
+                                             locatario, livros);
+        aluguel.setLocatario(locatario);
+        aluguel.setLivros(livros);
+        return aluguel;
+    }
+
+    public static List<Aluguel> listaAluguel(Locatario locatario, List<Livro> livros) {
+        var aluguelAtivo = createAluguel(locatario, livros);
+        aluguelAtivo.setStatus(AluguelStatus.ATIVO);
+
+        var aluguelFinalizado = createAluguel(locatario, livros);
+        aluguelFinalizado.setStatus(AluguelStatus.FINALIZADO);
+
+        var aluguelCancelado = createAluguel(locatario, livros);
+        aluguelCancelado.setStatus(AluguelStatus.CANCELADO);
+
+        return List.of(aluguelAtivo, aluguelFinalizado, aluguelCancelado);
+    }
+}
